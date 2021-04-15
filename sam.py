5d898aee1d1a293c792ad312d0651cff4b6f3d82
PRaliphada <PRaliphada@icloud.com>
2021-04-15 21:50:05 +0200

    sdhfs

---
+++ sam.py
@@ -0,0 +1,23 @@
+dhgbdfgbdjbfbvhjfgvbidfvbsdfjsfn f
+jngbfjgbdsfbvdf[v
+'d'
+vbn fkjbn bnsd,b'
+d.
+df.d'
+b
+sfb.sf
+gn.
+sfg.n
+sgn.
+s.n
+'s.n
+s.n
+s.n
+s.n
+.s
+n.h
+n.h'n,f
+jm,fj;m,hgnlkfgnjbfjvbfdv,g'b;
+dfn
+dm,ln,md
+hm
